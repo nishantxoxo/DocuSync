af990a2a6b75c8408e48cc6b202ebac28987c91c
nishant <nishantpriya.scorpion@gmail.com>
2026-01-27 14:41:45 +0530

    google signin setup

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:docu_sync/colors.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -7,7 +8,15 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton.icon(onPressed:() {}, label: "", icon: ,),
+        child: ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(150, 50),
+            backgroundColor: kWhiteColor
+          ),
+          onPressed: () {},
+          label: Text("Sign in with Google", style:  TextStyle( color: kBlackColor),),
+          icon: Image.asset('assets/images/g-logo-2.png', height: 20,),
+        ),
       ),
     );
   }
